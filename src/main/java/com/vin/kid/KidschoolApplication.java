c4a0569cef89dc85992f4dbfb336ce45cfd4fedc
vinay verma <vinayvermalmp@gmail.com>
2024-01-24 21:19:20 +0530

    first commit

---
+++ src/main/java/com/vin/kid/KidschoolApplication.java
@@ -0,0 +1,13 @@
+package com.vin.kid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vin/kid/KidschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vin.kid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
