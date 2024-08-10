838316e6ceb094f6df5e7ebdb10bff7996d67e2e
vinay verma <vinayvermalmp@gmail.com>
2024-08-10 16:24:31 +0530

    exception

---
+++ src/main/java/com/vin/kid/exception/Test.java
@@ -0,0 +1,16 @@
+package com.vin.kid.exception;
+
+
+/*
+Core Java - OOPS Concepts, Collections, Exceptions and Multi Threading
+Spring - IOC, DI, Types of Injections, Spring AOP, SPRING JDBC…
+Coding example - Solving a problem using Java Code
+Design Patterns - Singleton, Factory, Abstract Factory, Facade Pattern.
+SQL Questions - Joins and Some Scenarios
+Hibernate - Basics.
+* */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("hello");
+    }
+}
--- src/main/java/com/vin/kid/model/BaseEntity.java
+++ src/main/java/com/vin/kid/model/BaseEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
